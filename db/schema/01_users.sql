c4d0a95d7ec618c193aae3db108f4ac53d853847
Sandeep Chopra <sandeep.chopra7@gmail.com>
2020-06-27 13:17:03 +0000

    adds create table ordered items

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -21,7 +21,6 @@ CREATE TABLE users
 );
 
 
-
 CREATE TABLE orders
 (
   id SERIAL PRIMARY KEY NOT NULL,
@@ -38,5 +37,8 @@ CREATE TABLE orders
 CREATE TABLE ordered_items
 (
   id SERIAL PRIMARY KEY NOT NULL,
-
+  order_id INTEGER REFERENCES orders(id) ON DELETE CASCADE,
+  menu_item_id INTEGER REFERENCES menu_items(id),
+  price_charged INTEGER NOT NULL,
+  qty INTEGER NOT NULL DEFAULT 0,
 );
