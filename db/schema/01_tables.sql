a7e7ee8587e60f066b54881fe80fc2375c739006
Sandeep Chopra <sandeep.chopra7@gmail.com>
2020-06-27 14:34:58 +0000

    adds seed data for orders, order_item & menu_items

--- db/schema/01_tables.sql
+++ db/schema/01_tables.sql
@@ -29,8 +29,7 @@ CREATE TABLE orders
   special_instructions TEXT,
   order_ready_duration INTEGER NOT NULL,
   order_ready BOOLEAN NOT NULL DEFAULT FALSE,
-  order_complete_at TIMESTAMP NOT NULL,
-  order_total as INTEGER
+  order_complete_at TIMESTAMP,
 );
 
 
@@ -40,7 +39,7 @@ CREATE TABLE ordered_items
   order_id INTEGER REFERENCES orders(id) ON DELETE CASCADE,
   menu_item_id INTEGER REFERENCES menu_items(id),
   price_charged INTEGER NOT NULL,
-  qty INTEGER NOT NULL DEFAULT 0,
+  qty INTEGER NOT NULL DEFAULT 1,
 );
 
 CREATE TABLE menu_items
@@ -48,6 +47,8 @@ CREATE TABLE menu_items
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255),
   thumbnail_url VARCHAR(255),
-  price INTEGER,
-  description TEXT
+  price INTEGER NOT NULL,
+  description TEXT,
+  category VARCHAR
+  (255) NOT NULL
 );
--- db/seeds/01_seeds.sql
+++ db/seeds/01_seeds.sql
@@ -38,3 +38,35 @@ VALUES
   (
     'Nell Medina', 'juliansantos@aol.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(575
 ) 912-7053');
+
+
+INSERT INTO orders
+  (user_id, order_placed_at, special_instructions, order_ready_duration)
+VALUES
+  (1, 539940155, 'No plastic cutlery', 15);
+
+INSERT INTO ordered_items
+  (order_id, menu_item_id, price_charged, qty)
+VALUES
+  (1, 3, 750, 2),
+  (1, 4, 1299, 1),
+  (1, 2, 1399, 1),
+  (1, 6, 1199, 1),
+  (1, 3, 120, 4);
+
+
+INSERT INTO menu_items
+  (name,thumbnail_url, price, description,category)
+VALUES
+  ('香茅牛尾 Braised Oxtail with lemon grass', 'https://bit.ly/2VmXl7d', 2350, 'Chefs Recommendation Braised Oxtail with lemon grass', 'chef'),
+  ('XO Sauce Seafood Fried Rice Noodle', 'https://bit.ly/2VmXl7d', 1399, 'Chef Recommendation XO Sauce Seafood Fried Rice Noodle', 'chef'),
+  ('燒雞脾 BBQ Chicken Leg', 'https://bit.ly/2VmXl7d', 750, 'Whole slow roasted honey bbq chicken leg, chopped and served with a side of ginger onion sauce.', 'appetizers'),
+  ('磅燒雞翼 BBQ Chicken Wing', 'https://bit.ly/2VmXl7d', 1299, 'One lb of slow roasted honey bbq chicken wings.', 'appetizers'),
+  ('Assorted Meat on Rice(雜燴飯)', 'https://bit.ly/2VmXl7d', 1299, 'Seafood , meat, veggies mixed on rice with oysters sauce.', 'mains'),
+  ('Chicken and broccoli on rice(西蘭花雞片飯)', 'https://bit.ly/2VmXl7d', 1199, 'Wok fried chicken and brocolli on rice', 'mains'),
+  ('四川雞飯 Schezwan Style Chicken on Rice', 'https://bit.ly/2VmXl7d', 1150, 'Schezwan Style Chicken on Rice in exotic spices & marinade', 'mains'),
+  ('Pork Chop in Black Pepper', 'https://bit.ly/2VmXl7d', 1150, 'Pork Chop in Black Pepper Sauce on Rice with magic ingredients', 'mains'),
+  ('General tao chicken on rice (左雞飯)', 'https://bit.ly/2VmXl7d', 1099, 'General tao style white meat chicken wok fried with green pepper on bed of steam rice', 'mains'),
+  ('Canadian Short Can', 'https://bit.ly/2VmXl7d', 600, 'Cold Freezy Chilled Canadian Short Can', 'drinks'),
+  ('Soft Drink', 'https://bit.ly/2VmXl7d', 120, 'Cold Freezy Chilled Soft Drink Can', 'drinks'),
+  ('Water', 'https://bit.ly/2VmXl7d', 120, 'Cold Freezy Chilled Soft Water', 'drinks')
