4f19b459343a1ca807ee591ee4b912f7f7e46c5c
Sandeep Chopra <sandeep.chopra7@gmail.com>
2020-06-27 13:24:22 +0000

    adds create table menu_items

--- db/schema/01_tables.sql
+++ db/schema/01_tables.sql
@@ -42,3 +42,12 @@ CREATE TABLE ordered_items
   price_charged INTEGER NOT NULL,
   qty INTEGER NOT NULL DEFAULT 0,
 );
+
+CREATE TABLE menu_items
+(
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255),
+  thumbnail_url VARCHAR(255),
+  price INTEGER,
+  description TEXT
+);
