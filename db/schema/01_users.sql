68679672181029ebc6590eacb5c744280f838ae7
Sandeep Chopra <sandeep.chopra7@gmail.com>
2020-06-27 12:52:12 +0000

    cleans up schema dir & add drop tables instructinos

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,12 @@
 -- Drop and recreate Users table (Example)
 
-DROP TABLE IF EXISTS users CASCADE;
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
-);
+DROP TABLE IF EXISTS users
+CASCADE;
+DROP TABLE IF EXISTS orders
+CASCADE;
+DROP TABLE IF EXISTS ordered_items
+CASCADE;
+DROP TABLE IF EXISTS menu_items
+CASCADE;
+
+
