fb3edfa2b630260137925987a27446c4e536e4c4
Sandeep Chopra <sandeep.chopra7@gmail.com>
2020-06-27 13:30:19 +0000

    adds seed data for users

---
+++ db/seeds/01_seeds.sql
@@ -0,0 +1,40 @@
+
+INSERT INTO users
+  (
+  name, email, password, phone)
+VALUES
+  (
+    'Devin Sanders', 'tristanjacobs@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(255) 633-6919');
+INSERT INTO users
+  (
+  name, email, password, phone)
+VALUES
+  (
+    'Iva Harrison', 'allisonjackson@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(517
+) 311-5301');
+INSERT INTO users
+  (
+  name, email, password, phone)
+VALUES
+  (
+    'Lloyd Jefferson', 'asherpoole@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(308) 441-4630');
+INSERT INTO users
+  (
+  name, email, password, phone)
+VALUES
+  (
+    'Dale Coleman', 'michaelgray@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(450) 547-7689');
+INSERT INTO users
+  (
+  name, email, password, phone)
+VALUES
+  (
+    'Alejandro Osborne', 'ariaatkinson@outlook.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(387
+) 647-6695');
+INSERT INTO users
+  (
+  name, email, password, phone)
+VALUES
+  (
+    'Nell Medina', 'juliansantos@aol.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(575
+) 912-7053');
