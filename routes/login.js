4b29c4ad8b5f76fd6058106eb7c128475e33a355
Sandeep Chopra <sandeep.chopra7@gmail.com>
2020-06-29 23:09:52 +0000

    adds error view & user authentication work in progress

--- routes/login.js
+++ routes/login.js
@@ -7,5 +7,32 @@ module.exports = () => {
   router.get("/", (req, res) => {
     res.render("login");
   });
+
+  router.post("/", (req, res) => {
+    const { email, password } = req.body;
+    //if user is trying to login without filling details
+    if (!email || !password) {
+      res.status(400).render("error", {
+        error: "400. Please provide valid credentials.",
+        user: undefined,
+      }); //check if the user is registed or not
+    } else if (!isEmailRegistered(users, email)) {
+      res.status(403).render("error", {
+        error: "403. User does not exist.",
+        user: undefined,
+      }); //if user is registed than check if password is matching or not
+    } else if (bcrypt.compareSync(password, getHashedPassword(users, email))) {
+      const userID = getUserIDByEmail(users, email);
+      req.session.user_id = userID;
+      res.redirect(`/urls`);
+    } else {
+      //if not, return to user with error
+      res.status(403).render("error", {
+        error: "Password and email do not match.",
+        user: undefined,
+      });
+    }
+  });
+
   return router;
 };
