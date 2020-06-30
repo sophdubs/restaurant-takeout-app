f6a9814c93870e15ec30b8461ce185dd6225129b
Sophie <sophiemargueritedubois@gmail.com>
2020-06-30 22:32:53 +0000

    added hard coded values to display a pending order card

--- routes/admin.js
+++ routes/admin.js
@@ -4,35 +4,51 @@ const router = express.Router();
 module.exports = ({ fetchPendingOrderDetails, fetchPendingOrders }) => {
   router.get("/", (req, res) => {
     const templateVars = {};
-    fetchPendingOrders()
-      .then(pendingOrders => {
-        // pendingOrders is an array of objects which includes order_id, user_id, placed_at, special_instructions
-        templateVars.pendingOrders = JSON.parse(pendingOrders);
+    res.render('admin');
+    // fetchPendingOrders()
+    //   .then(pendingOrders => {
+    //     // pendingOrders is an array of objects which includes order_id, user_id, placed_at, special_instructions
+    //     templateVars.pendingOrders = JSON.parse(pendingOrders);
 
-        // let pendingIds = JSON.parse(pendingOrders).map(order => order.order_id);
+    //     // let pendingIds = JSON.parse(pendingOrders).map(order => order.order_id);
 
-        fetchPendingOrderDetails()
-          .then(pendingOrderDetails => {
-            console.log(JSON.parse(pendingOrderDetails));
-            res.send('ok');
-          });
-
-
-
+    //     fetchPendingOrderDetails()
+    //       .then(pendingOrderDetails => {
+    //         console.log(JSON.parse(pendingOrderDetails));
+    //         res.send('ok');
+    //       });
 
 
-        // console.log(templateVars.pendingOrders.order_id);
-        // templateVars[pendingOrders] = pendingOrders;
-        // res.render('admin', templateVars);
-        // fetchPendingOrderDetails()
-        //  .then(pendingOrderDetails => {
-        //  });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-    });
+
+
+
+    //     // console.log(templateVars.pendingOrders.order_id);
+    //     // templateVars[pendingOrders] = pendingOrders;
+    //     // res.render('admin', templateVars);
+    //     // fetchPendingOrderDetails()
+    //     //  .then(pendingOrderDetails => {
+    //     //  });
+    //   })
+    //   .catch(err => {
+    //     res
+    //       .status(500)
+    //       .json({ error: err.message });
+    // });
   });
+  router.post("/", (req, res) => {
+    console.log(req.body);
+    const {order_id, wait_time} = req.body;
+    if (wait_time === 0) {
+      // Order is ready => notify guest and change status in order to 'ready'
+      // .then
+      res.redirect("admin");
+    } else {
+      // Order is confirmed => notify guest, update wait time and ready_at in db, update status to 'confirmed' in db
+      // .then
+      res.redirect("admin");
+    }
+  });
+
+
   return router;
 };
