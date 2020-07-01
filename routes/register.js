07076a8a13eac5740e38882cac14438d4e7466a0
Sandeep Chopra <sandeep.chopra7@gmail.com>
2020-07-01 16:45:04 +0000

    fixes issues caused by previous branch

--- routes/register.js
+++ routes/register.js
@@ -4,32 +4,37 @@ const router = express.Router();
 module.exports = ({ registerUser }) => {
   router.get("/", (req, res) => {
     templateVars = {
-      errorMsg: null
+      errorMsg: null,
     };
     res.render("register", templateVars);
   });
 
   router.post("/", (req, res) => {
-    const {userName, userEmail, userPhone, userPassword} = req.body;
+    const { userName, userEmail, userPhone, userPassword } = req.body;
     // If user submits with missing inputs, redirect back to register page with error message
     if (!userName || !userEmail || !userPhone || !userPassword) {
       let templateVars = {
-        errorMsg: 'Please fill out all fields before submitting'
+        errorMsg: "Please fill out all fields before submitting",
       };
-      res.render("register", templateVars)
-    };
-    const values = [userName, userEmail, userPhone, userPassword, 'customer'];
+      res.render("register", templateVars);
+    }
+
+    //if user is already registed
+
+    getUserByEmail(userEmail).then(function (res) {
+      console.log(res);
+    });
+
+    const values = [userName, userEmail, userPhone, userPassword, "customer"];
     registerUser(values)
-      .then(newUser => {
+      .then((newUser) => {
         req.session.user_id = newUser.id;
         req.session.role = newUser.role;
         res.redirect("menu");
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-    });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
   });
   return router;
 };
